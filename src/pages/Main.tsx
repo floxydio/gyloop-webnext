fe014600f466d3cfd4b83e2143d13893ee839397
marcellinoputra <marcellinoputra@github.com>
2023-08-28 20:07:53 +0700

    product sub menu

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,25 +2,27 @@ import Head from 'next/head';
 import Footer from '../app/components/Footer/Footer';
 import { useEffect } from 'react';
 import MainComponent from '@/app/components/Main/MainComponent';
-import Header from '@/app/components/Header/Header';
+import Header from '@/app/components/Header/header';
 
 export default function Main() {
-
   useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js")
-  })
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+  });
 
   return (
     <>
-        <Head>
+      <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
 
         <title>Gyloop | Make Business Connected</title>
-        </Head>
+      </Head>
 
       <Header />
       <MainComponent />
