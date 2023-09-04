6306ac713466535c3234ba48d1d0643b0699daec
marcellinoputra <marcellinoputra@github.com>
2023-09-04 10:10:01 +0700

    middleware next-intl update

--- src/app/middleware/middleware.ts
+++ src/app/middleware/middleware.ts
@@ -3,7 +3,7 @@ import createMiddleware from 'next-intl/middleware';
 const storageLocale = localStorage.getItem("lang")
 
 export default createMiddleware({
-    locales: ['en', 'id'],
+    locales: ['en', 'id', 'deu', 'esp', 'fr', 'ita', 'ned', 'por'],
 
     defaultLocale: storageLocale === undefined ? "en" : `${storageLocale}`,
     localePrefix: 'always'
