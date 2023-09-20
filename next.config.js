987d39e765671431b71c3c9ee320fa4af5a65ded
floxydio <diooktar@gmail.com>
2023-09-20 17:06:31 +0700

    update next js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     env: {
         REACT_DEV_URL: "http://localhost:3000",
         REACT_PROD_URL: "https://nextjs-portfolio-website-ten.vercel.app",
-    },
+    },  
     compiler: {
         reactRemoveProperties: true,
     },
