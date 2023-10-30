b0b021ff4d906391bd6effde14cef873811174c5
floxydio <diooktar@gmail.com>
2023-10-31 00:50:29 +0700

    bug: update font by nextjs

--- src/app/components/NextHead/NextSEO.tsx
+++ src/app/components/NextHead/NextSEO.tsx
@@ -16,6 +16,7 @@ export default function NextSEO({ seoHead }: { seoHead: HeadPropsSEO }) {
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+      <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
       <meta name="description" content={seoHead.metaDescription} />
       <meta name="keywords" content={seoHead.metaKeywords} />
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,131 @@
-import {NextIntlClientProvider} from 'next-intl';
- 
-export default function App({Component, pageProps}) {
+import { NextIntlClientProvider } from 'next-intl';
+import localFont from 'next/font/local'
+
+
+/*
+@font-face {
+  font-family: "Compact-Ultralight";
+  src: url("../../public/font/SFCompactDisplay-Ultralight.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Compact-Thin";
+  src: url("../../public/font/SFCompactDisplay-Thin.woff");
+  font-display: swap;
+}
+@font-face {
+  font-family: "Compact-Light";
+  src: url("../../public/font/SFCompactDisplay-Light.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Compact-Regular";
+  src: url("../../public/font/SFCompactDisplay-Regular.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Compact-Medium";
+  src: url("../../public/font/SFCompactDisplay-Medium.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Compact-Semibold";
+  src: url("../../public/font/SFCompactDisplay-Semibold.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Compact-Bold";
+  src: url("../../public/font/SFCompactDisplay-Bold.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Compact-Heavy";
+  src: url("../../public/font/SFCompactDisplay-Heavy.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Compact-Black";
+  src: url("../../public/font/SFCompactDisplay-Black.woff");
+  font-display: swap;
+
+}
+@font-face {
+  font-family: "Pro-Regular";
+  src: url("../../public/font/SFProText-Regular.woff");
+  font-display: swap;
+
+}
+
+*/
+
+const font = localFont({
+  src: [
+    {
+      path: '../../public/font/SFCompactDisplay-Ultralight.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Thin.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Light.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Semibold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Heavy.woff',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFCompactDisplay-Black.woff',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/font/SFProText-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    }
+  ]
+})
+
+export default function App({ Component, pageProps }) {
   return (
     <NextIntlClientProvider messages={pageProps.messages}>
-      <Component {...pageProps} />
+      <main className={font.className}>
+        <Component {...pageProps} />
+      </main>
     </NextIntlClientProvider>
   );
 }
